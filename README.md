6de0da48764087ad9dcfd8c79ea018a1f7a150cd
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2024-04-02 13:35:06 +0300

    simplify script by getting TSV (txt) version. Update (87 new prefixes added since 10-Apr-2023)

--- README.md
+++ README.md
@@ -26,8 +26,4 @@ up- and insert URIs based on a prefix.
 The prefix list is pulled semi-regularly from http://prefix.cc/.
 - Use the perl script `rdf-prefix-update.pl` to get a new version, then replace the variable `rdf-prefix-alist`
 - TODO: reimplement as elisp function `rdf-prefix-update`
-<<<<<<< HEAD
-- Last updated: 2023-03-21
-=======
-- Last updated: 10-Apr-2023
->>>>>>> d1970d3 (update prefixes)
+- Last updated: 2-Apr-2024 (87 new prefixes added since 10-Apr-2023)
--- rdf-prefix-update.pl
+++ rdf-prefix-update.pl
@@ -4,15 +4,17 @@
 # TODO: reimplement as elisp function rdf-prefix-update
 
 use LWP::Simple;
-my $url = "https://prefix.cc/popular/all";
+my $url = "https://prefix.cc/popular/all.file.txt";
 my $prefixes = get($url);
 die "Couldn't GET $url\n" unless $prefixes;
 
 my %prefix;
-while ($prefixes =~ m{resource="(.*?)" .*? href="(.*?)"}g) {
-  $prefix{$2} = $1;
+for (split /\n/,$prefixes) {
+  my ($k,$v) = split/\t/;
+  $prefix{$k} = $v;
 };
 
+binmode (STDOUT, ":encoding(UTF-8)");
 print "(defvar rdf-prefix-alist\n";
 my $n = 0;
 for (sort keys %prefix) {
